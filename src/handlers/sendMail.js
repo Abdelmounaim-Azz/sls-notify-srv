9f5f6e14ba39ba7c6d9532e38a8cb12e64c1143d
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-25 10:25:10 +0100

    send a test mail sanity check

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,31 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({region: "eu-west-1"});
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "abdelmounaim.azziza@outlook.fr",
+    Destination: {
+      ToAddresses: ["azz.sahafrica@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "test email",
+        },
+      },
+      Subject: {
+        Data: "test email",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
